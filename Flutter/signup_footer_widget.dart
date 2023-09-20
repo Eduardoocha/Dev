7ac27a6212c98c385a9a5dac4e21ba7a3304a8b6
Eduardo Fagundes Rocha <142803216+Eduardoocha@users.noreply.github.com>
2023-09-19 22:28:42 -0300

    h

---
+++ Flutter/signup_footer_widget.dart
@@ -0,0 +1,35 @@
+class SignUpFooterWidget extends StatelessWidget {
+  const SignUpFooterWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("OR"),
+        SizedBox(
+          width: double.infinity,
+          child: OutlinedButton.icon(
+            onPressed: () {},
+            icon: const Image(
+              image: AssetImage(tGoogleLogoImage),
+              width: 20.0,
+            ),
+            label: Text(tSignInWithGoogle.toUpperCase()),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text.rich(TextSpan(children: [
+            TextSpan(
+              text: tAlreadyHaveAnAccount,
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            TextSpan(text: tLogin.toUpperCase())
+          ])),
+        )
+      ],
+    );
+  }
+}
