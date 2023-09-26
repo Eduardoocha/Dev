6340a2841661b8a27001ae92e1564f32dedcb922
Eduardoocha <142803216+Eduardoocha@users.noreply.github.com>
2023-09-26 11:25:06 -0300

    26/09/2023

---
+++ Exercicios Funçoes/calculo.js
@@ -0,0 +1,7 @@
+    function Div(a, b){
+    return a / b;
+    }
+    var num1 = 0, num2 = 0
+
+    num1 = parseInt(prompt('Digite um número', ''));
+    num2 = parseInt(prompt('Digite outro número', ''));
